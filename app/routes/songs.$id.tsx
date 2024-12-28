c792ba247b9de5e43d6efc180c7e4c6df35d5cb9
Andrew Thompson <6337878+Mrwerdo@users.noreply.github.com>
2024-12-28 19:03:52 +1100

    fix rendering flash

--- app/routes/songs.$id.tsx
+++ app/routes/songs.$id.tsx
@@ -113,7 +113,7 @@ export default function Songs() {
           {
             !song.bars ? null : song.bars.map((value, index) => {
               const isActive = (metronome.bar?.id ?? 0) == value.id;
-              const currentBar = Math.floor((metronome.counter - metronome.totalCountUntilStartOfBar) / (metronome.numberOfBeats * metronome.numberOfSubBeats));
+              const currentBar = Math.max(0, Math.floor((metronome.counter - metronome.totalCountUntilStartOfBar) / (metronome.numberOfBeats * metronome.numberOfSubBeats)));
               return <Bar key={index} bar={value} isActive={isActive} currentBar={currentBar} />
             })
           }
